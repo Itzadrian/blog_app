cc12469fe7ba62366bc04add3e38c482aef35d2c
Dominic Nzube Simeon <simeondominic@gmail.com>
2023-06-15 12:59:16 +0100

    first commit

---
+++ v1/app.js
@@ -0,0 +1,61 @@
+var express = require("express"),
+ app = express(),
+ bodyParser = require("body-parser"),
+ mongoose = require("mongoose");
+// APP CONFIG
+mongoose.connect("mongodb://127.0.0.1:27017/blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+var blogschema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogschema);
+
+//ROUTES
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+// INDEX ROUTE
+app.get("/blogs", async function(req, res){
+    try {
+        var blogs = await Blog.find({});
+        res.render("index", {blogs: blogs});
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+})
+
+// CREATE ROUTE
+
+app.post("/blogs", async function(req, res){
+    // Get Data From Form
+    var title = req.body.title;
+    var image = req.body.image;
+    var body = req.body.body;
+    var newblog = {title: title, image: image, body: body};
+    // Create New Blog
+    try {
+        var newblog = await Blog.create(newblog);
+        res.redirect("/blogs");
+    }
+    catch(err){
+        res.redirect("new")
+        console.log(err);
+    }
+});
+app.listen(3000, function(){
+    console.log("Blog Server Started");
+});
